f7a664b4747591f8db1f9afd1fe1c01a216eede1
Ravnholt7507 <Andreas.ravnholt99@gmail.com>
2021-12-01 19:03:57 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Eksamensopgave
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+      
+}
